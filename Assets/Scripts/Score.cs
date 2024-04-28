9a493704e3deec4425f001023a9dc2a9f653c961
Neo Te Aika <neo.teaika@gmail.com>
2024-04-28 18:47:18 +1200

    woah

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+
+    private int score = 0;
+
+    private float waitTime = 1.0f; // Wait this amount of time before generating a new obstacle
+    private float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = score.ToString() + " Preorders";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > waitTime)
+        {
+            score += 10;
+
+            scoreText.text = score.ToString() + " Preorders";
+
+            timer = timer - waitTime; // Resets the timing interval
+        }
+    }
+}
--- Assets/Scripts/Scoreboard_Scripts/ScoreboardEntryUI.cs
+++ Assets/Scripts/Scoreboard_Scripts/ScoreboardEntryUI.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 namespace Sno.Scoreboards
 {
+<<<<<<< Updated upstream
     public class ScoreboardEntryUI : MonoBehaviour
+=======
+    public class ScoreboardEntryUI
+>>>>>>> Stashed changes
     {
         [SerializeField] private TextMeshProUGUI entryNameText = null;
         [SerializeField] private TextMeshProUGUI entryScoreText = null;
